908d5502d320c40fda59aac49cc8b18fff265f7d
Manas <mkadyrbaev@gmail.com>
2021-01-27 17:17:18 +0100

    Update draw_window func

--- main.py
+++ main.py
@@ -5,11 +5,20 @@ WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("PinPong Game")
 
 YELLOW = (255,255,0)
+RED = (139,0,0)
+GREEN = (0,100,0)
+
+BORDER = pygame.Rect()
 
 FPS = 60
 
+TIGER_IMAGE
+FROG_IMAGE
+
 def draw_window():
     WIN.fill(YELLOW)
+    pagame.draw_rect(WIN, RED, BORDER)
+    pagame.draw_rect(WIN, GREEN, BORDER)
     pygame.display.update()
 
 
