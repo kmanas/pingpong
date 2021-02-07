4258a54da90922f95e9e550bf932e2e21edb8336
Manas <mkadyrbaev@gmail.com>
2021-02-07 21:41:26 +0100

    Create images

--- main.py
+++ main.py
@@ -1,24 +1,26 @@
 import pygame
+import os
 
 WIDTH, HEIGHT = 900, 500
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("PinPong Game")
+pygame.display.set_caption("PinPong - Good Luck!")
 
-YELLOW = (255,255,0)
+YELLOW = (255,255,0) #Screen Background
 RED = (139,0,0)
 GREEN = (0,100,0)
 
-BORDER = pygame.Rect()
+#BORDER = pygame.Rect()
 
 FPS = 60
 
-TIGER_IMAGE
-FROG_IMAGE
+
 
 def draw_window():
     WIN.fill(YELLOW)
-    pagame.draw_rect(WIN, RED, BORDER)
-    pagame.draw_rect(WIN, GREEN, BORDER)
+    
+    RED_IMAGE = pygame.draw.rect(WIN, RED, (WIDTH/10 - 50, HEIGHT/2 - 50, WIDTH/90, HEIGHT/10))  #(x,y,width, height)
+    GREEN_IMAGE = pygame.draw.rect(WIN, GREEN, (WIDTH - 40, HEIGHT/2 - 50, WIDTH/90, HEIGHT/10))
+    
     pygame.display.update()
 
 
